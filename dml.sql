651c049aa2b515db430f4586c64fc8d56baf762a
Ermoshin Mikhail <138512261+ermachine@users.noreply.github.com>
2025-01-14 01:52:43 +0300

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -8,46 +8,45 @@ LEFT JOIN (
     WHERE course_name = 'Базы данных'
 ) t2
     ON t1.course_id = t2.course_id
-LEFT JOIN group t3
-    ON t1.default_group_id = t2.group_id
+LEFT JOIN group_table t3
+    ON t1.default_group_id = t3.group_id
 ;
 
 
 -- 2
 SELECT DISTINCT
-    g.head_student_name AS head_student_name
+    t2.head_student_name AS head_student_name
 FROM schedule t1
-INNER JOIN group t2
-    ON g.group_id = sch.default_group_id
+INNER JOIN group_table t2
+    ON t2.group_id = t1.default_group_id
 INNER JOIN teacher_course t3
-    ON sch.course_id = tc.course_id
+    ON t1.course_id = t3.course_id
 INNER JOIN (
     SELECT employee_name
     FROM employee
-    WHERE employee_name = 'Иванов Александр Максимович'
+    WHERE employee_name = 'Павлов Марат Манасович'
 ) t4
-    ON tc.teacher_name = e.employee_name
+    ON t1.teacher_name = t4.employee_name
 ;
 
 
 -- 3
 SELECT DISTINCT
-    a.auditorium_number as auditorium_number,
-    a.building_number as building_number
+    t1.auditorium_number as auditorium_number,
+    t1.building as building
 FROM auditorium t1
 INNER JOIN (
     SELECT
         auditorium_id,
         course_id
     FROM schedule
-    WHERE DAYOFWEEK(sch.start_dt) = 2
-    -- Функция DAYOFWEEK() возвращает значения от 1 (воскресенье) до 7 (суббота), поэтому понедельник будет 2
+    WHERE TO_CHAR(DATE '2023-03-20', 'D') = '2'
 ) t2
     ON t1.auditorium_id = t2.auditorium_id
 INNER JOIN (
     SELECT course_id
     FROM course
-    WHERE course_name = 'Теория вероятностей'
+    WHERE course_name = 'Теория вероятности'
 ) t3
     ON t2.course_id = t3.course_id
 ;
@@ -59,7 +58,17 @@ SELECT
 FROM (
     SELECT
         employee_name
-    FROM employee_history
+    FROM (
+        SELECT
+            employee_name,
+            department_name
+        FROM employee_history
+        UNION ALL
+        SELECT
+            employee_name,
+            department_name
+        FROM employee
+    )
     GROUP BY employee_name
     HAVING count(distinct department_name) > 1
 );
